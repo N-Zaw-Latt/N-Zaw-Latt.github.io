872dee2e4f2c96f3215bcbd73d185e4ebf8512e5
Nea <108646962+N-Zaw-Latt@users.noreply.github.com>
2025-08-27 14:47:38 +0700

    Update README.js

--- README.js
+++ README.js
@@ -1,5 +1,14 @@
-<div>
-  <h1>
-  "HI"
-  <\h1>
-<\div>
+<html>
+<body>
+
+<h1>My First Web Page</h1>
+<p>My First Paragraph</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+</script>
+
+</body>
+</html>
